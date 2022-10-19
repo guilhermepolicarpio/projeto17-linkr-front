7a7644ba402424ff07f2d6681afa96891f4680e4
Alice Lipski <alipski@pop-os.localdomain>
2022-10-19 16:39:18 -0300

    homepage layout finished

--- src/layouts/Post.js
+++ src/layouts/Post.js
@@ -1,66 +1,209 @@
 import styled from "styled-components";
 
-export default function Post(){
-
-    return(
-        <Wrapper>
-            <div>
-                <img src='https://www.dictionary.com/e/wp-content/uploads/2018/03/chibi.jpg'/>
-            </div>
-            <div>
-                <h3>Fulano de Tal</h3>
-                <h4>Muito maneiro esse tutorial de Material UI com React, deem uma olhada!</h4>
-            </div>
-        </Wrapper>
-    )
+export default function Post() {
+  return (
+    <Wrapper>
+      <div>
+        <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/chibi.jpg" />
+      </div>
+      <div>
+        <h3>Fulano de Tal</h3>
+        <h4>
+          Muito maneiro esse tutorial de Material UI com React, deem uma olhada!
+        </h4>
+        <Metadata>
+          <div>
+            <h2>Como aplicar o Material UI em um projeto React</h2>
+            <h4>
+              Hey! I have moved this tutorial to my personal blog. Same content,
+              new location. Sorry about making you click through to another
+              page.
+            </h4>
+            <p>https://medium.com/@pshrmn/a-simple-react-router</p>
+          </div>
+          <div>
+            <img src="https://images.genius.com/267629b35cc1e6d6b8acd6532a638bd7.700x700x1.jpg" />
+          </div>
+        </Metadata>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
+  display: flex;
+  background-color: #000000;
+  height: fit-content;
+  width: 100%;
+  border-radius: 15px;
+  margin: 15px 0;
+  padding: 15px;
+
+  & > div > img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  h3 {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 19px;
+    margin-bottom: 12px;
+    color: #ffffff;
+  }
+
+  h4 {
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 17px;
+    margin-bottom: 12px;
+    color: #b7b7b7;
+    line-height: 20px;
+  }
+
+  div:nth-child(1) {
     display: flex;
-    background-color: #000000;
-    height: fit-content;
-    width: 100%;
-    border-radius: 15px;
-    margin: 15px 0;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 15%;
+  }
 
-    img {
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-    }
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 90%;
+    padding: 10px 35px 20px 10px;
+  }
 
-    h3 {
-        font-family: 'Lato', sans-serif;
-        font-weight: 400;
-        font-size: 19px;
-        margin-bottom: 16px;
-        color: #FFFFFF;
-    }
-
-    
-    h4 {
-        font-family: 'Lato', sans-serif;
-        font-weight: 400;
-        font-size: 17px;
-        margin-bottom: 16px;
-        color: #B7B7B7;
-    }
-
-    div:nth-child(1) {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-        width: 10%;
-        padding: 15px;
-    }
+  @media only screen and (max-width: 600px) {
+    border-radius: 0;
+    padding-bottom: 8px;
 
     div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-start;
-        width: 90%;
-        padding: 25px 25px 25px 0;
+        padding-right: 10px;
     }
-`
+  }
+`;
+
+const Metadata = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+  border: 1px solid grey;
+  border-radius: 5px;
+  overflow: auto;
+
+  div:nth-child(1) {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: auto;
+    height: 200px;
+    padding: 15px;
+  }
+
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    width: 200px;
+    padding: 0;
+    object-fit: cover;
+  }
+
+  h2 {
+    font-family: "Lato", sans-serif;
+    width: 100%;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 20px;
+    color: #cecece;
+  }
+
+  h4 {
+    width: 100%;
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    color: #9b9595;
+    margin: 0;
+    line-height: 19px;
+  }
+
+  p {
+    width: 100%;
+    font-family: "Lato", sans-serif;
+    font-weight: 400;
+    font-size: 17px;
+    color: #cecece;
+    line-height: 20px;
+    text-decoration: underline;
+  }
+
+  img {
+    height: 200px;
+    width: 200px;
+    border-radius: 0 5px 5px 0;
+    object-fit: cover;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    height: 120px;
+    h2 {
+        font-size: 14px;
+        line-height: 14px;
+    }
+
+    h4, p {
+        font-size: 14px;
+        line-height: 14px;
+    }
+
+    img {
+        height: 120px;
+         width: 120px;
+    }
+    div:nth-child(1) {
+        height: 120px;
+        padding: 8px;
+    }
+    div:nth-child(2) {
+        height: 120px;
+        width: 120px;
+    }
+  }
+
+  @media only screen and (max-width: 600px) {
+    height: 120px;
+    h2 {
+        font-size: 12px;
+        line-height: 12px;
+    }
+
+    h4, p {
+        font-size: 12px;
+        line-height: 12px;
+    }
+
+    img {
+        height: 120px;
+         width: 100px;
+    }
+    div:nth-child(1) {
+        height: 120px;
+        padding: 8px;
+    }
+    div:nth-child(2) {
+        height: 120px;
+        width: 100px;
+    }
+  }
+`;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,156 +2,249 @@ import styled from "styled-components";
 import Header from "../layouts/Header";
 import Post from "../layouts/Post";
 
-export default function Home(){
-    return(
-        <Wrapper>
-            <Header />
-            <Feed>
-                <h1>timeline</h1>
-                <Create>
-                    <div>
-                        <img src='https://www.dictionary.com/e/wp-content/uploads/2018/03/chibi.jpg'/>
-                    </div>
-                    <div>
-                        <h3>What are you going to share today?</h3>
-                        <input type='url' placeholder="http://..."></input>
-                        <textarea placeholder="Awesome article about #javascript"></textarea>
-                        <button>Publish</button>
-                    </div>
-                </Create>
-                <Post />
-            </Feed>
-        </Wrapper>
-    );
+export default function Home() {
+  return (
+    <Wrapper>
+      <Header />
+      <div>
+        <Feed>
+          <h1>timeline</h1>
+          <Create>
+            <div>
+              <img src="https://www.dictionary.com/e/wp-content/uploads/2018/03/chibi.jpg" />
+            </div>
+            <div>
+              <h3>What are you going to share today?</h3>
+              <input type="url" placeholder="http://..."></input>
+              <textarea placeholder="Awesome article about #javascript"></textarea>
+              <button>Publish</button>
+            </div>
+          </Create>
+          <Post />
+          <Post />
+          <Post />
+        </Feed>
+        <Sidebar>
+          <h1>trending</h1>
+          <hr />
+          <div>
+            <p>#javascript</p>
+            <p>#react</p>
+            <p>#nodejs</p>
+            <p>#python</p>
+          </div>
+        </Sidebar>
+      </div>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    display: flex;
-	flex-direction: column;
-	width: 100%;
-	height: 100%;
-	min-height: 100vh;
-	align-items: center;
-	justify-content: center;
-`
-const Feed = styled.div`
-    display: flex;
-	flex-direction: column;
-	width: 55%;
-	height: 100%;
-	min-height: 100vh;
-	align-items: center;
-	justify-content: start;
-    padding-top: 160px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: center;
 
-    h1 {
-        display: flex;
-        justify-content: flex-start;
-        align-items: flex-start;
+  & > div:nth-child(2) {
+    display: flex;
+    width: 75%;
+    height: fit-content;
+    align-items: flex-start;
+    justify-content: flex-start;
+    padding-top: 160px;
+    column-gap: 25px;
+  }
+
+  @media only screen and (max-width: 600px) {
+    & > div:nth-child(2) {
         width: 100%;
-        font-family: 'Oswald', sans-serif;
-        font-weight: 700;
-        font-size: 43px;
-        color: #FFFFFF;
-        margin-bottom: 25px;
     }
-`
+  }
+`;
+
+const Feed = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  height: 100%;
+  min-height: 100vh;
+  align-items: center;
+  justify-content: start;
+
+  h1 {
+    display: flex;
+    justify-content: flex-start;
+    align-items: flex-start;
+    width: 100%;
+    font-family: "Oswald", sans-serif;
+    font-weight: 700;
+    font-size: 43px;
+    color: #ffffff;
+    margin-bottom: 25px;
+  }
+
+  @media only screen and (max-width: 1000px) {
+    width: 85%;
+  }
+
+  @media only screen and (max-width: 800px) {
+    width: 100%;
+  }
+
+  @media only screen and (max-width: 600px) {
+    width: 100%;
+    h1 {
+      padding-left: 20px;
+    }
+  }
+`;
 
 const Create = styled.div`
-    display: flex;
-    background-color: #FFFFFF;
-    height: fit-content;
+  display: flex;
+  background-color: #ffffff;
+  height: fit-content;
+  width: 100%;
+  border-radius: 15px;
+  margin: 25px 0;
+
+  img {
+    width: 60px;
+    height: 60px;
+    border-radius: 50%;
+  }
+
+  h3 {
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-size: 20px;
+    margin-bottom: 16px;
+    color: #707070;
+  }
+
+  input {
     width: 100%;
-    border-radius: 15px;
-    margin: 25px 0;
+    margin: 5px 0;
+    border-radius: 5px;
+    border: none;
+    background-color: #efefef;
+    padding: 10px;
+    color: #000000;
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+  }
 
-    img {
-        width: 60px;
-        height: 60px;
-        border-radius: 50%;
-    }
+  textarea {
+    width: 100%;
+    margin: 5px 0;
+    border-radius: 5px;
+    border: none;
+    background-color: #efefef;
+    padding: 10px;
+    color: #000000;
+    resize: none;
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+    min-height: 90px;
+  }
 
-    h3 {
-        font-family: 'Lato', sans-serif;
-        font-weight: 300;
-        font-size: 20px;
-        margin-bottom: 16px;
-        color: #707070;
-    }
+  input::placeholder {
+    color: #949494;
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+  }
 
-    input {
-        width: 100%;
-        margin: 5px 0;
-        border-radius: 5px;
-        border: none;
-        background-color: #EFEFEF;
-        padding: 10px;
-        color: #000000;
-        font-family: 'Lato', sans-serif;
-        font-weight: 300;
-        font-size: 15px;
-    }
+  textarea::placeholder {
+    color: #949494;
+    font-family: "Lato", sans-serif;
+    font-weight: 300;
+    font-size: 15px;
+  }
 
-    textarea {
-        width: 100%;
-        margin: 5px 0;
-        border-radius: 5px;
-        border: none;
-        background-color: #EFEFEF;
-        padding: 10px;
-        color: #000000;
-        resize: none;
-        font-family: 'Lato', sans-serif;
-        font-weight: 300;
-        font-size: 15px;
-        min-height: 90px;
-    }
+  div:nth-child(1) {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    width: 15%;
+    padding: 15px;
+    min-width: 60px;
+  }
 
-    input::placeholder {
-        color: #949494;
-        font-family: 'Lato', sans-serif;
-        font-weight: 300;
-        font-size: 15px;
-    }
+  div:nth-child(2) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 85%;
+    padding: 25px 25px 25px 0;
+  }
 
-    textarea::placeholder {
-        color: #949494;
-        font-family: 'Lato', sans-serif;
-        font-weight: 300;
-        font-size: 15px;
-    }
+  button {
+    width: 120px;
+    align-self: flex-end;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 5px;
+    border: none;
+    padding: 10px 20px;
+    color: #ffffff;
+    font-family: "Lato", sans-serif;
+    font-weight: 700;
+    font-size: 14px;
+    background-color: #1877f2;
+  }
 
-    div:nth-child(1) {
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-        width: 10%;
-        padding: 15px;
-    }
+  @media only screen and (max-width: 600px) {
+    border-radius: 0;
 
     div:nth-child(2) {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: flex-start;
-        width: 90%;
-        padding: 25px 25px 25px 0;
+      padding: 10px 10px;
     }
+  }
+`;
 
-    button {
-        width: 120px;
-        align-self: flex-end;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 5px;
-        border: none;
-        padding: 10px 20px;
-        color: #FFFFFF;
-        font-family: 'Lato', sans-serif;
-        font-weight: 700;
-        font-size: 14px;
-        background-color: #1877F2;
-    }
-`
+const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 20%;
+  height: fit-content;
+  align-items: flex-start;
+  justify-content: flex-start;
+  margin-top: 90px;
+  background-color: #000000;
+  color: #ffffff;
+  border-radius: 15px;
+  min-width: 130px;
+
+  h1 {
+    padding: 14px 20px;
+    font-family: "Oswald", sans-serif;
+    font-weight: 700;
+    font-size: 27px;
+  }
+
+  & > div {
+    padding: 14px 20px;
+    font-family: "Lato", sans-serif;
+    font-weight: 700;
+    font-size: 19px;
+    line-height: 20px;
+  }
+
+  hr {
+    border: solid 1px #484848;
+    width: 100%;
+    margin: 0;
+  }
+
+  @media only screen and (max-width: 800px) {
+    display: none;
+  }
+`;
